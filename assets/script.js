eaea3eac215cbc1a1450b5b0d984fe9373e86917
DJ Hersh <DanielJHersh@gmail.com>
2021-01-02 16:49:18 -0800

    Moved the javascript to the javascript file

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,16 @@
+(function() {
+    emailjs.init('user_HRpuwBHjAKteVvrKCOZrz');
+})();
+
 $("#contact-form").on("submit", event => {
     event.preventDefault();
-})
+    $("#contact_number").val(Math.random() * 100000 | 0);
+    emailjs.sendForm('service_c7rxoqk', 'template_dia2ck5', event.currentTarget).then(() => {
+        console.log("success!");
+    }, error => {
+        console.log("Failed...", error);
+    });
+    $("#nameInput").val("");
+    $("#emailInput").val("");
+    $("#messageInput").val("");
+});
